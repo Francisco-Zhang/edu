d565eee4159a42b5a83c96ce2cfab801df94c25e
张兴柱 <mr.francisco@foxmail.com>
2017-11-11 22:48:18 +0800

    spring boot 环境搭建，测试环境搭建

---
+++ src/test/java/com/edu/BaseTest.java
@@ -0,0 +1,13 @@
+package com.edu;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BookShopApplication.class)
+@Transactional
+public class BaseTest {
+}
---
+++ src/test/java/com/edu/FirstTest.java
@@ -0,0 +1,11 @@
+package com.edu;
+
+import org.junit.Test;
+
+public class FirstTest extends BaseTest {
+
+    @Test
+    public  void  hello(){
+        System.out.println("Spring hello");
+    }
+}
